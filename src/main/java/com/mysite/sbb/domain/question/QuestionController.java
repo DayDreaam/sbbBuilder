fe2a9204f658ee612ed48d790b3adf92dad26b74
daydreaam <dayingyeo@naver.com>
2024-12-11 11:20:10 +0900

    redirect

--- src/main/java/com/mysite/sbb/domain/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/domain/question/QuestionController.java
@@ -1,17 +1,23 @@
 package com.mysite.sbb.domain.question;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @RequestMapping("/question")
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
+    private final QuestionRepository questionRepository;
     @GetMapping("/list")
-    @ResponseBody
-    public String list(){
-        return "question list";
+    public String list(Model model){
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList",questionList);
+        return "question_list";
     }
 }
