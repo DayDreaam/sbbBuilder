2976e703940f30cca5dca6e77fde0368ad877d2c
daydreaam <dayingyeo@naver.com>
2024-12-28 22:32:46 +0900

    3-14 진행중

--- src/main/java/com/mysite/sbb/domain/answer/AnswerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mysite.sbb.domain.answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-}
